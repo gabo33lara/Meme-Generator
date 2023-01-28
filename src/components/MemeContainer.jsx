a8266cbc769e7296b883cc554c9dc8c4b6564ceb
Leonardo Lara <gabo33lara@gmail.com>
2023-01-28 17:39:23 -0400

    fixing public path for imgs

--- src/components/MemeContainer.jsx
+++ src/components/MemeContainer.jsx
@@ -5,7 +5,7 @@ function MemeContainer({memeLineTop, memeLineBottom, memeImg}) {
         <div className="meme-container">
             <p className={memeImg === 'select-meme' ? 'line-top none' : 'line-top'.trimEnd() }>{memeLineTop}</p>
             <p className={memeImg === 'select-meme' ? 'line-bottom none' : 'line-bottom'.trimEnd() }>{memeLineBottom}</p>
-            <img src={`/img/${memeImg}.jpg`} alt="Selected meme" className={memeImg === 'select-meme' ? 'none' : '' } />
+            <img src={`${process.env.PUBLIC_URL}/img/${memeImg}.jpg`} alt="Selected meme" className={memeImg === 'select-meme' ? 'none' : '' } />
             <h3 className={memeImg !== 'select-meme' ? 'none' : '' }>Qué mirás bobo?<br /><span>Selecciona un meme bobo!</span></h3>
         </div>
     );
